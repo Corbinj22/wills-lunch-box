12b33d3cbbd96b048f8669a123c1faef2f54afa4
Justin Corbin <jcorbin2290@gmail.com>
2020-01-05 14:54:27 -0700

    initial commit

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,27 @@
-class LunchBox {
+var Snack = require('./snack');
 
+class LunchBox {
+constructor(lunchbox) {
+ this.owner = lunchbox.owner;
+ this.madeOf = lunchbox.madeOf;
+ this.shape = lunchbox.shape;
+ this.color = lunchbox.color;
+ this.handle = true;
+ this.snacks = [];
+ this.healthySnacks = [];
+}
+
+acquireSnack(broughtSnacks) {
+this.snacks.push(broughtSnacks);
+broughtSnacks.isInLunchBox = true;
+if (broughtSnacks.healthy == true) {
+  this.healthySnacks.push(broughtSnacks);
+  }
+}
+
+findHealthySnacks(broughtSnacks) {
+ return this.healthySnacks;
+}
 }
 
 module.exports = LunchBox;
